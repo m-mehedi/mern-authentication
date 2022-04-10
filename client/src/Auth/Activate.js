b83b9e0746756184588ecc17dd97ef9dcd3e7ace
MEHEDI <me.mehedi@hotmail.com>
2022-04-10 19:35:29 +0600

    account verification with web token via frontend

--- client/src/Auth/Activate.js
+++ client/src/Auth/Activate.js
@@ -6,6 +6,7 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.min.css";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
+import jwtDecode from 'jwt-decode'
 
 const Activate = () => {
   const params = useParams();
@@ -17,44 +18,46 @@ const Activate = () => {
 
   useEffect(() => {
     let token = params.token;
-    console.log(token);
+    let { name } = jwtDecode(token);
+    // console.log(name);
+    if (token) {
+      setValues({ ...values, name, token });
+    }
   }, []);
 
   const { name, token, show } = values;
 
   const clickSubmit = (event) => {
     event.preventDefault();
-    setValues({ ...values, buttonText: "Submitting.." });
     axios({
       method: "POST",
-      url: `${process.env.REACT_APP_API}/signup`,
+      url: `${process.env.REACT_APP_API}/account-activation`,
       data: { token },
     })
       .then((response) => {
-        console.log("ACTIVATE SUCCESS", response);
+        console.log("ACCOUNT ACTIVATEION SUCCESS", response);
         setValues({
           ...values,
-          name: "",
-          email: "",
-          password: "",
-          buttonText: "Submitted",
+          show: false
         });
         toast.success(response.data.message);
       })
       .catch((error) => {
-        console.log("ACTIVATE ERROR", error.response.data);
-        setValues({ ...values, buttonText: "Submit" });
+        console.log("ACCOUNT ACTIVATEION ERROR", error.response.data.error);
         toast.error(error.response.data.error);
       });
   };
 
   const activationLink = () => (
     <div>
-      <h1 className="p-5 text-center">
-        Hey {name}, Please activate your account!
-      </h1>
+        <Box display="flex" alignItems="center"
+        justifyContent="center">
+        <h3>
+            Hey {name}, Please activate your account!
+        </h3>
+        </Box>
 
-      <Box sx={{ p: 1 }} display="flex" justifyContent="flex-center">
+      <Box sx={{ p: 1 }} display="flex" m="auto">
         <Button variant="contained" color="success" onClick={clickSubmit}>
           Activate Account
         </Button>
